6db31d8862afd52b0f0614f4393c682e72633f42
Amanda Skinner <amandamuscha@yahoo.com>
2023-06-29 17:51:02 -0400

    adding dist subtree3

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,2 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/MyRecordCollection/" : "/",
-  };
+export const publicPath = process.env.NODE_ENV === "production" ? "/MyRecordCollection/" : "/";
 //https://learnvue.co/articles/deploy-vue-to-github-pages
